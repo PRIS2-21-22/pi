a5a9b3837d100969bb7c9136ecf82f0a9ef6c41d
daniporre <daniporre@hotmail.es>
2021-03-24 19:08:47 +0100

    Cambio de nombre a los metodos para cumplir con SonarCloud

--- vector.py
+++ vector.py
@@ -6,40 +6,40 @@ class Vector:
     def __init__(self, punto1 ,punto2):
         self.punto1= punto1
         self.punto2 = punto2
-        
+
     
-    def cX(self):
+    def CoordenadaX(self):
         return self.punto2.get_x() - self.punto1.get_x()
 
-    def cY(self):
+    def CoordenadaY(self):
         return self.punto2.get_y() - self.punto1.get_y()
 
-    def moduloVector(self):
+    def ModuloVector(self):
         return math.sqrt(math.pow(self.cX(),2) + math.pow(self.cY(),2))
 
-    def productovectorialVector(self,vector):
+    def ProductovectorialVector(self,vector):
         return self.cX()*vector.cY()- self.cY*vector.cX()
 
-    def ecuacionDeLaRecta(self):
+    def EcuacionDeLaRecta(self):
         #ecuacion: y=[(y2-y1)/(x2-x1)]*(x-x1)+y1
         return f"y = {((self.punto2.get_y()-self.punto1.get_y())/(self.punto2.get_x()-self.punto1.get_x()))}*(x-{self.punto1.get_x()}) + {self.punto1.get_y()}"
 
-    def ecuacionDeLaRecta2(self, m, b):
+    def EcuacionDeLaRecta2(self, m, b):
         #ecuacion: y = mx+b
         return f"y = {m}*x + {b}"
     
-    def ecuacionDeLaRecta3(self, m, punto):
+    def EcuacionDeLaRecta3(self, m, punto):
         #ecuacion: y=m*(x-x1)+y1
         return f"y = {m}*(x-{punto.get_x()}) + {punto.get_y()}"
 
-    def ecuacionDeLaRecta4(self, a, b, c):
+    def EcuacionDeLaRecta4(self, a, b, c):
         #ecuacion: a*x+b*y+c=0
         return f"{a}*x + {b}*y + {c} = 0"
     
-    def ecuacionDeLaRecta5(self, x0):
+    def EcuacionDeLaRecta5(self, x0):
         #ecuacion: x=x0
         return f"x = {x0}"
     
-    def ecuacionDeLaRecta6(self, y0):
+    def EcuacionDeLaRecta6(self, y0):
         #ecuacion: y=y0
         return f"y = {y0}"
