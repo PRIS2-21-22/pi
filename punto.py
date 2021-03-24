5a90e8f406315490967557e7108990f2269416f9
daniporre <daniporre@hotmail.es>
2021-03-24 16:11:54 +0100

    Avances en vector y punto

--- punto.py
+++ punto.py
@@ -3,13 +3,6 @@ class Punto():
     def __init__(self,x,y):
         self.x = x
         self.y = y
-    
-    def set_coord(self,cordX, cordY):
-        self.x = cordX
-        self.y = cordY
-
-    def toString(self):
-        return "("+str(self.x)+","+str(self.y)+")"
 
     def get_x(self):
         return self.x
--- vector.py
+++ vector.py
@@ -20,6 +20,25 @@ class Vector:
         return self.cX()*vector.cY()- self.cY*vector.cX()
 
     def ecuacionDeLaRecta(self):
-       return f"y = {((self.punto2.get_y()-self.punto1.get_y())/(self.punto2.get_x()-self.punto1.get_x()))}*(x-{self.punto1.get_x()}) + {self.punto1.get_y()}"
+        #ecuacion: y=[(y2-y1)/(x2-x1)]*(x-x1)+y1
+        return f"y = {((self.punto2.get_y()-self.punto1.get_y())/(self.punto2.get_x()-self.punto1.get_x()))}*(x-{self.punto1.get_x()}) + {self.punto1.get_y()}"
 
+    def ecuacionDeLaRecta2(self, m, b):
+        #ecuacion: y = mx+b
+        return f"y = {m}*x + {b}"
     
+    def ecuacionDeLaRecta3(self, m, punto):
+        #ecuacion: y=m*(x-x1)+y1
+        return f"y = {m}*(x-{punto.get_x()}) + {punto.get_y()}"
+
+    def ecuacionDeLaRecta4(self, a, b, c):
+        #ecuacion: a*x+b*y+c=0
+        return f"{a}*x + {b}*y + {c} = 0"
+    
+    def ecuacionDeLaRecta5(self, x0):
+        #ecuacion: x=x0
+        return f"x = {x0}"
+    
+    def ecuacionDeLaRecta6(self, y0):
+        #ecuacion: y=y0
+        return f"y = {y0}"
